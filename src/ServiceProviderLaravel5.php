0e9bd94df60062e48a8c5cd87242581dc7c165ad
Markus <markus@tenghamn.com>
2017-03-02 15:23:37 +0100

    Fixes problems I created :)

--- src/ServiceProviderLaravel5.php
+++ src/ServiceProviderLaravel5.php
@@ -14,7 +14,7 @@ class ServiceProviderLaravel5 extends \Illuminate\Support\ServiceProvider
      */
     public function boot()
     {
-        $this->app->singleton('maknz.slack', function($app);
+        $this->publishes([__DIR__.'/config/config.php' => config_path('slack.php')]);
     }
 
     /**
@@ -26,7 +26,7 @@ class ServiceProviderLaravel5 extends \Illuminate\Support\ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__.'/config/config.php', 'slack');
 
-        $this->app['maknz.slack'] = $this->app->share(function ($app) {
+        $this->app->singleton('maknz.slack', function($app) {
             return new Client(
                 $app['config']->get('slack.endpoint'),
                 [
